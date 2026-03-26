27a034f91e62e7b017718030c89ed20272afeb56
Maktis <pospisilmatysek@gmail.com>
2026-03-26 15:36:54 +0100

    prepare Shop.java for further usage in NPC.java

--- src/Items/Shop.java
+++ src/Items/Shop.java
@@ -1,9 +1,26 @@
 package Items;
 
+import java.util.ArrayList;
+
 public class Shop {
 
+    private ArrayList<Item> items;
 
 
+    public ArrayList<Item> getItems() {
+        return items;
+    }
 
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
+    }
 
+    public Item findItem(ItemNames name){
+        for (Item item : items){
+            if(item.getName() == name){
+                return item;
+            }
+        }
+        return null;
+    }
 }
