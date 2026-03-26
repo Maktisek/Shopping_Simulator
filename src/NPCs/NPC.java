d9577dd6b693ec3a4e33709c4dbf636ded69d26d
Maktis <pospisilmatysek@gmail.com>
2026-03-26 15:38:11 +0100

    add method which loads the demand of the NPC.

--- src/NPCs/NPC.java
+++ src/NPCs/NPC.java
@@ -1,6 +1,8 @@
 package NPCs;
 
 import Items.Item;
+import Items.Shop;
+import Player.Player;
 
 import java.util.ArrayList;
 
@@ -11,11 +13,21 @@ public class NPC {
     private ArrayList<Item> items;
     private Item[] demand;
 
-
-
-
-
-
+    public void loadDemand(Player player, Shop shop) {
+        double first = 0;
+        double second = 0;
+        for (int i = 0; i < this.items.size(); i++) {
+            double s = (player.findItem(items.get(i).getName()).getAmount() * quantityWeight) +
+                    ((shop.findItem(items.get(i).getName()).getPrice() / player.findItem(items.get(i).getName()).getAverageBuyPrice()) * convenienceWeight);
+            if (s > first) {
+                first = s;
+                demand[0] = items.get(i);
+            } else if (s > second) {
+                second = s;
+                demand[1] = items.get(i);
+            }
+        }
+    }
 
     public int getQuantityWeight() {
         return quantityWeight;
