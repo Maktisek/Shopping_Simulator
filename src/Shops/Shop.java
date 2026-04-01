b2db10ae271b5c676959f8171d3beb8f5e2989e9
Maktis <pospisilmatysek@gmail.com>
2026-04-01 21:21:37 +0200

    add a system of updating the entire shop after setting a new day.

--- src/Shops/Shop.java
+++ src/Shops/Shop.java
@@ -4,6 +4,7 @@ import Items.Item;
 import Items.ItemNames;
 import Items.WrongItemException;
 import NPCs.NPC;
+import Player.Player;
 
 public class Shop {
 
@@ -16,8 +17,22 @@ public class Shop {
         this.npc.loadItems(this.items);
     }
 
-    public void newDay(){
+    public void newDay(Player player) throws WrongItemException{
+        updateItems();
+        updateNPC(player);
+    }
 
+    private void updateItems(){
+        for (Item item : items){
+            item.updatePrice();
+            item.updatePenalization(-0.1);
+            item.resetCurrentDayAmount();
+        }
+    }
+
+    private void updateNPC(Player player) throws WrongItemException{
+        this.npc.loadDemand(player,this);
+        this.npc.setNewPrices(player, this);
     }
 
     public Item findItem(ItemNames name){
