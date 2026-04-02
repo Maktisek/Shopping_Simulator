6e87930d16db93ff5ee2d317b7da180168d32059
Maktis <pospisilmatysek@gmail.com>
2026-04-02 13:20:21 +0200

    add important methods into ShopManagement.java

--- src/Shops/ShopManagement.java
+++ src/Shops/ShopManagement.java
@@ -1,4 +1,42 @@
 package Shops;
 
+import Items.WrongItemException;
+import Player.Player;
+
+import java.util.ArrayList;
+
 public class ShopManagement {
+
+    private ArrayList<Shop> shops;
+
+    public ShopManagement() {
+        this.shops = new ArrayList<>();
+    }
+
+    public Shop findShop(ShopNames name){
+        for (Shop shop : shops){
+            if(shop.getName() == name){
+                return shop;
+            }
+        }
+        return null;
+    }
+
+    public void setNewDays(Player player) {
+        for (Shop shop : shops){
+            try {
+                shop.newDay(player);
+            }catch (WrongItemException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public ArrayList<Shop> getShops() {
+        return shops;
+    }
+
+    public void setShops(ArrayList<Shop> shops) {
+        this.shops = shops;
+    }
 }
