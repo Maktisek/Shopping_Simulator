983964201c0e47834dddcd993e721358f35f48f2
Maktis <pospisilmatysek@gmail.com>
2026-04-03 12:51:33 +0200

    fill all upgrades with necessary methods and fields

--- src/Upgrade/MaxBoughtUpgrade.java
+++ src/Upgrade/MaxBoughtUpgrade.java
@@ -2,16 +2,26 @@ package Upgrade;
 
 public class MaxBoughtUpgrade implements Upgrade{
 
+    private int maxBoughtPossible;
+    private int price;
+
     public MaxBoughtUpgrade() {
+        this.maxBoughtPossible = 100;
+        this.price = 50;
     }
 
     @Override
     public void levelUp() {
-
+        this.maxBoughtPossible += 10;
     }
 
     @Override
     public int dataInfo() {
-        return 0;
+        return maxBoughtPossible;
+    }
+
+    @Override
+    public int priceInfo() {
+        return this.price;
     }
 }
--- src/Upgrade/MaxSoldUpgrade.java
+++ src/Upgrade/MaxSoldUpgrade.java
@@ -1,18 +1,28 @@
 package Upgrade;
 
-public class MaxSoldUpgrade implements Upgrade{
+public class MaxSoldUpgrade implements Upgrade {
+
+    private int maxSoldPossible;
+    private int price;
 
     public MaxSoldUpgrade() {
+        this.maxSoldPossible = 100;
+        this.price = 100;
     }
 
 
     @Override
     public void levelUp() {
-
+        this.maxSoldPossible += 10;
     }
 
     @Override
     public int dataInfo() {
-        return 0;
+        return maxSoldPossible;
+    }
+
+    @Override
+    public int priceInfo() {
+        return this.price;
     }
 }
