94d401e4e024badc03ebd74c71b786460ee6651a
Maktis <pospisilmatysek@gmail.com>
2026-04-03 11:33:14 +0200

    add new class ShopKey.java

---
+++ src/Shops/ShopKey.java
@@ -0,0 +1,26 @@
+package Shops;
+
+public class ShopKey {
+
+    private int price;
+    private boolean unlocked;
+
+    public ShopKey() {
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public boolean isUnlocked() {
+        return unlocked;
+    }
+
+    public void setUnlocked(boolean unlocked) {
+        this.unlocked = unlocked;
+    }
+}
