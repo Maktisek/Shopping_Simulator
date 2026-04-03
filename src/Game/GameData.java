6ffca141b9680f7e14aff2af0de3f7ebbb0def85
Maktis <pospisilmatysek@gmail.com>
2026-04-03 12:08:47 +0200

    add fields into GameData.java

--- src/Game/GameData.java
+++ src/Game/GameData.java
@@ -1,4 +1,13 @@
 package Game;
 
+import DayCycle.DayManagement;
+import Player.Player;
+import Shops.ShopManagement;
+
 public class GameData {
+
+    private Player player;
+    private DayManagement dayManagement;
+    private ShopManagement shopManagement;
+
 }
