f89cc190345a22ef7cd3ce86e819b49d42150be0
Maktis <pospisilmatysek@gmail.com>
2026-04-03 12:44:10 +0200

    add loading method into UpgradeSystem.java

--- src/Upgrade/UpgradeSystem.java
+++ src/Upgrade/UpgradeSystem.java
@@ -1,4 +1,20 @@
 package Upgrade;
 
+import java.util.HashMap;
+
 public class UpgradeSystem {
+
+    private final HashMap<UpgradeNames, Upgrade> upgrades;
+
+    public UpgradeSystem() {
+        this.upgrades = new HashMap<>();
+        loadUpgrades();
+    }
+
+    private void loadUpgrades(){
+        this.upgrades.put(UpgradeNames.BUY, new MaxBoughtUpgrade());
+        this.upgrades.put(UpgradeNames.SELL, new MaxSoldUpgrade());
+    }
+
+
 }
