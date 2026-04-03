3d1df1623e63108c5c98fe19dcd89b607f966964
Maktis <pospisilmatysek@gmail.com>
2026-04-03 11:43:17 +0200

    add name of the day into Day.java

--- src/DayCycle/Day.java
+++ src/DayCycle/Day.java
@@ -2,17 +2,13 @@ package DayCycle;
 
 public class Day {
 
+    private DayNames dayName;
     private final int number;
     private final int dayIncome;
 
     public Day(int number) {
         this.number = number;
-        dayIncome = 0;
-    }
-
-    public Day(int number, int dayIncome) {
-        this.number = number;
-        this.dayIncome = dayIncome;
+        this.dayIncome = 0;
     }
 
     public int getNumber() {
@@ -23,11 +19,11 @@ public class Day {
         return dayIncome;
     }
 
-    @Override
-    public String toString() {
-        return "Day{" +
-                "number=" + number +
-                ", dayIncome=" + dayIncome +
-                '}';
+    public DayNames getDayName() {
+        return dayName;
+    }
+
+    public void setDayName(DayNames dayName) {
+        this.dayName = dayName;
     }
 }
