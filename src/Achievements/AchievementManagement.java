5952a280f1fd40a2884324ce8b30ccd793d38300
Maktis <pospisilmatysek@gmail.com>
2026-04-04 13:29:53 +0200

    add all fields into AchievementManagement.java

--- src/Achievements/AchievementManagement.java
+++ src/Achievements/AchievementManagement.java
@@ -1,4 +1,48 @@
 package Achievements;
 
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class AchievementManagement {
+
+    private HashMap<AchievementTypes, ArrayList<Achievement>> possibleAchievements;
+    private HashMap<AchievementTypes, ArrayList<Achievement>> doneAchievements;
+    private ArrayList<Achievement> loadedAchievements;
+
+    public AchievementManagement() {
+        this.possibleAchievements = new HashMap<>();
+        this.doneAchievements = new HashMap<>();
+        this.loadedAchievements = new ArrayList<>();
+    }
+
+    public void loadPossibleAchievements(){
+        for (int i = 0; i < AchievementTypes.values().length; i++) {
+
+        }
+    }
+
+    public ArrayList<Achievement> getLoadedAchievements() {
+        return loadedAchievements;
+    }
+
+    public void setLoadedAchievements(ArrayList<Achievement> loadedAchievements) {
+        this.loadedAchievements = loadedAchievements;
+    }
+
+    public HashMap<AchievementTypes, ArrayList<Achievement>> getPossibleAchievements() {
+        return possibleAchievements;
+    }
+
+    public void setPossibleAchievements(HashMap<AchievementTypes, ArrayList<Achievement>> possibleAchievements) {
+        this.possibleAchievements = possibleAchievements;
+    }
+
+    public HashMap<AchievementTypes, ArrayList<Achievement>> getDoneAchievements() {
+        return doneAchievements;
+    }
+
+    public void setDoneAchievements(HashMap<AchievementTypes, ArrayList<Achievement>> doneAchievements) {
+        this.doneAchievements = doneAchievements;
+    }
 }
