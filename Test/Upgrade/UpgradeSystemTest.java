a719c6b854b9c33513ac8f118ac8e63cde330f4a
Maktis <pospisilmatysek@gmail.com>
2026-04-08 20:17:23 +0200

    add UpgradeSystemTest

--- Test/Upgrade/UpgradeSystemTest.java
+++ Test/Upgrade/UpgradeSystemTest.java
@@ -1,7 +1,32 @@
 package Upgrade;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class UpgradeSystemTest {
 
+    UpgradeSystem upgradeSystem;
+
+    @Before
+    public void setUp() throws Exception {
+        this.upgradeSystem = new UpgradeSystem();
+    }
+
+    @Test
+    public void levelUpgradeTest(){
+        assertEquals(500, upgradeSystem.getUpgradeData(UpgradeNames.STOCK));
+        upgradeSystem.levelUpUpgrade(UpgradeNames.STOCK);
+        assertEquals(520, upgradeSystem.getUpgradeData(UpgradeNames.STOCK));
+        assertEquals(172, upgradeSystem.getUpgradePrice(UpgradeNames.STOCK));
+
+        upgradeSystem.newRebirth();
+
+        assertEquals(572, upgradeSystem.getUpgradeData(UpgradeNames.STOCK));
+
+        upgradeSystem.newRebirth();
+
+        assertEquals(624, upgradeSystem.getUpgradeData(UpgradeNames.STOCK));
+    }
 }
