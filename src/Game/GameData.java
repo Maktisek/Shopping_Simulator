dbaf732b8d874c5cee95996bfbd5896ac06c81d4
Maktis <pospisilmatysek@gmail.com>
2026-04-08 21:24:47 +0200

    add a constructor into GameData.java

--- src/Game/GameData.java
+++ src/Game/GameData.java
@@ -14,4 +14,11 @@ public class GameData {
     private UpgradeManagement upgradeManagement;
     private AchievementManagement achievementManagement;
 
+    public GameData(Player player, DayManagement dayManagement, ShopManagement shopManagement, UpgradeManagement upgradeManagement, AchievementManagement achievementManagement) {
+        this.player = player;
+        this.dayManagement = dayManagement;
+        this.shopManagement = shopManagement;
+        this.upgradeManagement = upgradeManagement;
+        this.achievementManagement = achievementManagement;
+    }
 }
