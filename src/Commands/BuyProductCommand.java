334d1aacbe78d17c46a6091a669e9f2d1ce1bc09
Maktis <pospisilmatysek@gmail.com>
2026-04-10 14:58:32 +0200

    finish new command - BuyProductCommand

--- src/Commands/BuyProductCommand.java
+++ src/Commands/BuyProductCommand.java
@@ -1,15 +1,38 @@
 package Commands;
 
+import Achievements.AchievementTypes;
 import Game.GameData;
+import Items.ItemNames;
+import Player.InvalidPlayerActionException;
+import Upgrade.UpgradeNames;
 
-public class BuyProductCommand extends Command{
+public class BuyProductCommand extends Command {
 
-    public BuyProductCommand(GameData gameData) {
+    private final int amount;
+    private int index;
+
+    public BuyProductCommand(GameData gameData, int index, int amount) {
         super(gameData);
+        this.amount = amount;
+        this.index = index;
     }
 
     @Override
     public String execute() {
-        return "";
+        if (!getDayManagement().getCurrentDay().canIncrementDayAmount(this.amount, getUpgradeManagement().getUpgradeData(UpgradeNames.BUY))) {
+            super.setSuccessful(false);
+            return "You cannot buy more than " + getUpgradeManagement().getUpgradeData(UpgradeNames.BUY) + " products at one day";
+        }
+        ItemNames product = getCurrentShop().getItems()[index].getItem().getName();
+        try {
+            getPlayer().buyItem(product, amount,getCurrentShop().findItem(product).getItem().getCurrentPrice());
+        }catch (InvalidPlayerActionException e){
+            super.setSuccessful(false);
+            return e.getMessage();
+        }
+        getCurrentShop().buyItem(index, amount);
+        getDayManagement().getCurrentDay().incrementDayAmount(amount);
+        getAchievementManagement().updateAchievement(AchievementTypes.BUY, amount);
+        return "Bought " + amount + "x " + product;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
+import Commands.BuyProductCommand;
 import Game.GameData;
 import Game.Initialization;
 
 public class Main {
     public static void main(String[] args) {
 
-
+        GameData gameData = new Initialization().getGameData();
+        System.out.println(new BuyProductCommand(gameData, 0, 2).execute());
+        System.out.println(gameData);
+    }
+    public static void initTest(){
         GameData gameData = new Initialization().getGameData();
         System.out.println(gameData.toString());
         gameData.getShopManagement().setNewDays(gameData.getPlayer());
