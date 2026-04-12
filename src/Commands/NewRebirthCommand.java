8e33a809e5067c77235d0a65f5bf77e56685913e
Maktis <pospisilmatysek@gmail.com>
2026-04-12 19:29:49 +0200

    add new command - NewRebirthCommand

---
+++ src/Commands/NewRebirthCommand.java
@@ -0,0 +1,23 @@
+package Commands;
+
+import Game.GameData;
+
+public class NewRebirthCommand extends Command{
+
+    public NewRebirthCommand(GameData gameData) {
+        super(gameData);
+    }
+
+    @Override
+    public String execute() {
+        int price = getUpgradeManagement().getRebirth().getPrice();
+        if(!getPlayer().canBuy(price)){
+            setSuccessful(false);
+            return "Not enough money";
+        }
+
+        getPlayer().setCurrentBalance(getPlayer().getCurrentBalance() - price);
+        getUpgradeManagement().setNewRebirth();
+        return "New rebirth";
+    }
+}
