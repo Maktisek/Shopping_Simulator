aa0b0cf92624caa4dde86d16bbb4037142a9f91e
Maktis <pospisilmatysek@gmail.com>
2026-04-16 12:54:51 +0200

    make the buttons pop up when the cursor is on them

--- src/UI/Buttons/TitleScreenButton.java
+++ src/UI/Buttons/TitleScreenButton.java
@@ -4,11 +4,14 @@ import UI.InvalidUILoadException;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.net.URL;
 
 public class TitleScreenButton extends JButton {
 
     private final Image img;
+    private boolean hoovered;
 
     public TitleScreenButton(String imgFile, int width, int height) throws InvalidUILoadException{
         super();
@@ -23,15 +26,48 @@ public class TitleScreenButton extends JButton {
         setContentAreaFilled(false);
         setBorderPainted(false);
 
-        Dimension dimension = new Dimension(width, height);
-        setMaximumSize(dimension);
-        setPreferredSize(dimension);
-        setMinimumSize(dimension);
+        setSizeOfButton(width,height);
+
+        setMouseListener();
     }
 
     @Override
     protected void paintComponent(Graphics g){
-        g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
         super.paintComponent(g);
+
+        int w = getWidth();
+        int h = getHeight();
+
+        if (hoovered) {
+            g.drawImage(img, 0, 0, w, h, this);
+        } else {
+            int offset = 5;
+            g.drawImage(img, offset, offset, w - (offset * 2), h - (offset * 2), this);
+        }
+    }
+
+    private void setMouseListener(){
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseExited(MouseEvent e) {
+                super.mouseExited(e);
+                TitleScreenButton.this.hoovered = false;
+                repaint();
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                super.mouseEntered(e);
+                TitleScreenButton.this.hoovered = true;
+                repaint();
+            }
+        });
+    }
+
+    private void setSizeOfButton(int width, int height){
+        Dimension dimension = new Dimension(width, height);
+        setMaximumSize(dimension);
+        setPreferredSize(dimension);
+        setMinimumSize(dimension);
     }
 }
