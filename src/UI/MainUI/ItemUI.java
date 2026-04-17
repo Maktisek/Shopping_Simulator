28fc8b86ecfca489f9ae21de1ce7748b5962f15d
Maktis <pospisilmatysek@gmail.com>
2026-04-17 19:35:22 +0200

    add a class for creating a label with stroke

--- src/UI/MainUI/ItemUI.java
+++ src/UI/MainUI/ItemUI.java
@@ -1,15 +1,19 @@
 package UI.MainUI;
 
-import Items.Item;
+
 import Items.ItemShop;
 import UI.BackgroundPanel;
 import UI.InvalidUILoadException;
 
+import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class ItemUI extends BackgroundPanel {
 
     private ItemShop item;
+    private Font currentFont;
 
     public ItemUI(String imgFile, ItemShop item) throws InvalidUILoadException {
         super(imgFile);
@@ -19,8 +23,43 @@ public class ItemUI extends BackgroundPanel {
         setMinimumSize(new Dimension(dimension));
         setPreferredSize(new Dimension(dimension));
         setMaximumSize(new Dimension(dimension));
+
+        initialize();
     }
 
+    private void initialize(){
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setOpaque(false);
+        setBorder(BorderFactory.createEmptyBorder(15, 0, 40, 0));
+        loadFont();
+        initializeLabel();
+    }
 
+    private void initializeLabel(){
+        StrokeLabel label = new StrokeLabel(this.item.getItem().getName().toString());
+        label.setFont(currentFont);
+        label.setOpaque(false);
+        label.setFont(this.currentFont.deriveFont(Font.BOLD,14.0f));
+        label.setForeground(Color.BLACK);
+
+        label.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        add(label);
+
+    }
+
+    private void loadFont(){
+        try {
+            InputStream in = getClass().getResourceAsStream("/Fonts/Daydream.otf");
+            Font customFont = Font.createFont(Font.TRUETYPE_FONT, in);
+
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(customFont);
+
+            this.currentFont = customFont;
+        } catch (IOException | FontFormatException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
---
+++ src/UI/MainUI/StrokeLabel.java
@@ -0,0 +1,57 @@
+package UI.MainUI;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.font.TextLayout;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+
+/**
+ * @author Google Gemini
+ */
+public class StrokeLabel extends JLabel {
+    private Color strokeColor = Color.WHITE;
+    private float strokeWidth = 4.5f;
+
+    public StrokeLabel(String text) {
+        super(text);
+        setBorder(new EmptyBorder(10, 10, 10, 10));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+
+        g2.setClip(null);
+
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+        String text = getText();
+        if (text == null || text.isEmpty()) {
+            g2.dispose();
+            return;
+        }
+
+        Font font = getFont();
+        TextLayout tl = new TextLayout(text, font, g2.getFontRenderContext());
+        Rectangle2D bounds = tl.getBounds();
+
+        Insets insets = getInsets();
+        double x = insets.left + (getWidth() - insets.left - insets.right - bounds.getWidth()) / 2 - bounds.getX();
+        double y = insets.top + (getHeight() - insets.top - insets.bottom - bounds.getHeight()) / 2 - bounds.getY();
+
+        Shape shape = tl.getOutline(AffineTransform.getTranslateInstance(x, y));
+
+        g2.setColor(strokeColor);
+        g2.setStroke(new BasicStroke(strokeWidth, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+        g2.draw(shape);
+
+        g2.setColor(getForeground());
+        g2.fill(shape);
+
+        g2.dispose();
+    }
+
+}
