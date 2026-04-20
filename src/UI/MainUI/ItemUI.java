9d927d2790c4ec7703802317808692a4b51ace8e
Maktis <pospisilmatysek@gmail.com>
2026-04-20 13:44:56 +0200

    remove unwanted font loading method from ItemUI.java, the font loading method from Important.java is used.

--- src/UI/MainUI/ItemUI.java
+++ src/UI/MainUI/ItemUI.java
@@ -47,7 +47,7 @@ public class ItemUI extends BackgroundPanel {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         setOpaque(false);
         setBorder(BorderFactory.createEmptyBorder(15, 0, 0, 0));
-        loadFont();
+        this.currentFont = Important.loadFont("/Fonts/Daydream.otf");
 
         initializeLabel();
         initializeImg();
@@ -151,20 +151,6 @@ public class ItemUI extends BackgroundPanel {
 //        label.setBorder(BorderFactory.createLineBorder(Color.RED)); //Debugging
 
         panel.add(label);
-    }
-
-    private void loadFont(){
-        try {
-            InputStream in = getClass().getResourceAsStream("/Fonts/Daydream.otf");
-            Font customFont = Font.createFont(Font.TRUETYPE_FONT, in);
-
-            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-            ge.registerFont(customFont);
-
-            this.currentFont = customFont;
-        } catch (IOException | FontFormatException e) {
-            e.printStackTrace();
-        }
     }
 
 }
