634c28ae9ee0b330b1d1deb636cae01077c2c685
Maktis <pospisilmatysek@gmail.com>
2026-04-22 16:45:40 +0200

    add new class - MyFrame

---
+++ src/UI/MainUI/MyFrame.java
@@ -0,0 +1,34 @@
+package UI.MainUI;
+
+import Game.GameData;
+import UI.InvalidUILoadException;
+import com.sun.tools.javac.Main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFrame {
+
+    private final JFrame frame;
+    private final GameData gameData;
+
+    public MyFrame(GameData gameData) throws InvalidUILoadException{
+        this.frame = new JFrame("Forest Market");
+        this.frame.setLayout(new BorderLayout());
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.setLocationRelativeTo(null);
+        this.frame.setResizable(false);
+        this.frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        this.gameData = gameData;
+        initialize();
+    }
+
+    private void initialize() throws InvalidUILoadException {
+        this.frame.add(new MainUI(gameData), BorderLayout.CENTER);
+    }
+
+    public void show() {
+        this.frame.setVisible(true);
+    }
+}
