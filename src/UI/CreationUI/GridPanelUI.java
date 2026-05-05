b80487ae5dafc81eb73cd7abf1b7293b513ae9e8
Maktis <pospisilmatysek@gmail.com>
2026-05-05 21:03:41 +0200

    make the grid for achievements work

--- src/UI/CreationUI/GridPanelUI.java
+++ src/UI/CreationUI/GridPanelUI.java
@@ -8,10 +8,12 @@ public class GridPanelUI extends JPanel {
     private JPanel grid;
     private final int cols;
     private final int gridWidth;
+    private final int height;
 
-    public GridPanelUI(int cols, int widthOfBox){
+    public GridPanelUI(int cols, int widthOfBox, int height){
         super();
         this.cols = cols;
+        this.height = height;
         this.gridWidth = (cols * widthOfBox) + ((cols - 1) * 20);
         initialize();
     }
--- src/UI/MainUI/AchievementManagementUI.java
+++ src/UI/MainUI/AchievementManagementUI.java
@@ -1,12 +1,21 @@
 package UI.MainUI;
 
+import Achievements.Achievement;
+import Achievements.AchievementTypes;
 import Game.GameData;
 import UI.CreationUI.BackgroundPanel;
 import UI.CreationUI.BarPanelUI;
+import UI.CreationUI.GridPanelUI;
+import UI.CreationUI.StrokeLabel;
+import UI.DialogUI.DialogUI;
 import UI.Exceptions.InvalidUILoadException;
+import UI.MainUI.StockUI.ItemPlayerUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+
+import static Utilities.Important.initializeScrollPane;
 
 public class AchievementManagementUI extends JPanel {
 
@@ -29,7 +38,7 @@ public class AchievementManagementUI extends JPanel {
 
     }
 
-    private void initializeLayerPane(){
+    private void initializeLayerPane() {
         this.layeredPane = new JLayeredPane();
         this.layeredPane.setLayout(new OverlayLayout(layeredPane));
 
@@ -42,6 +51,76 @@ public class AchievementManagementUI extends JPanel {
     private void initializeMainPanel() throws InvalidUILoadException {
         this.mainPanel = new BackgroundPanel("/MainUI/ShopUI/STOCK_UI.png");
         this.mainPanel.setLayout(new BorderLayout());
+        initializeGrid();
+    }
+
+    private void initializeGrid() throws InvalidUILoadException {
+        JPanel wrapper = new JPanel();
+        wrapper.setLayout(new BoxLayout(wrapper, BoxLayout.Y_AXIS));
+        wrapper.setOpaque(false);
+        fillGrid(wrapper);
+
+        JScrollPane scrollPane = initializeScrollPane(wrapper, 16);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder(135, 0, 0, 0));
+        mainPanel.add(scrollPane, BorderLayout.CENTER);
+    }
+
+    private void fillGrid(JPanel wrapper) throws InvalidUILoadException {
+        for (AchievementTypes type : AchievementTypes.values()) {
+            wrapper.add(Box.createVerticalStrut(20));
+            StrokeLabel typeLabel = new StrokeLabel(type.toString() + " achievements", 40.0f);
+            typeLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+            wrapper.add(typeLabel);
+            wrapper.add(Box.createVerticalStrut(20));
+
+            GridPanelUI gridPanelUI = new GridPanelUI(3, 300, 300);
+            gridPanelUI.setAlignmentX(Component.CENTER_ALIGNMENT);
+            fillDoneAchievements(gridPanelUI.getGrid(), gameData.getAchievementManagement().getDoneAchievements().get(type));
+            fillBlankSpots(gridPanelUI.getGrid(), type);
+            fillPossibleAchievements(gridPanelUI.getGrid(), gameData.getAchievementManagement().getPossibleAchievements().get(type));
+            gridPanelUI.finishGrid();
+            wrapper.add(gridPanelUI);
+        }
+    }
+
+    private void fillDoneAchievements(JPanel grid, ArrayList<Achievement> achievements) throws InvalidUILoadException {
+//        if(achievements != null) {
+//            for (Achievement achievement : achievements) {
+//                grid.add(new DialogUI("/MainUI/ShopUI/ITEMPLAYER_FRAME.png", "Test"));
+//            }
+//        }
+        for (int i = 0; i < 10; i++) {
+            ItemPlayerUI panel = new ItemPlayerUI("/MainUI/ShopUI/ITEMPLAYER_FRAME.png", gameData.getPlayer().findItem("Bread"));
+            Dimension dimension = new Dimension(300, 300);
+            panel.setPreferredSize(dimension);
+            panel.setMaximumSize(dimension);
+            panel.setMaximumSize(dimension);
+            grid.add(panel);
+        }
+    }
+
+    private void fillBlankSpots(JPanel grid, AchievementTypes type) throws InvalidUILoadException {
+        int leftOver = 3 - (10 % 3);
+        for (int i = 0; i < leftOver; i++) {
+            JPanel panel = new JPanel();
+            panel.setOpaque(false);
+            Dimension dimension = new Dimension(300, 300);
+            panel.setPreferredSize(dimension);
+            panel.setMaximumSize(dimension);
+            panel.setMaximumSize(dimension);
+            grid.add(panel);
+        }
+    }
+
+    private void fillPossibleAchievements(JPanel grid, ArrayList<Achievement> achievements) throws InvalidUILoadException {
+        for (int i = 0; i < 10; i++) {
+            ItemPlayerUI panel = new ItemPlayerUI("/MainUI/ShopUI/ITEMPLAYER_FRAME.png", gameData.getPlayer().findItem("Bread"));
+            Dimension dimension = new Dimension(300, 300);
+            panel.setPreferredSize(dimension);
+            panel.setMaximumSize(dimension);
+            panel.setMaximumSize(dimension);
+            grid.add(panel);
+        }
     }
 
     private void initializeSidePanel() throws InvalidUILoadException {
