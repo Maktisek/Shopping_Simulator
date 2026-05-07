88c1e4f9809ae2f9fb2b3b2d00869e6c9c13ea4e
Maktis <pospisilmatysek@gmail.com>
2026-05-07 20:05:28 +0200

    finish ClaimAchievementRewardCommand

--- src/Commands/AchievementCommands/ClaimAchievementRewardCommand.java
+++ src/Commands/AchievementCommands/ClaimAchievementRewardCommand.java
@@ -1,4 +1,27 @@
 package Commands.AchievementCommands;
 
-public class ClaimAchievementRewardCommand {
+import Achievements.Achievement;
+import Achievements.AchievementTypes;
+import Commands.Command;
+import Commands.CommandResult;
+import Commands.CommandState;
+import Game.GameData;
+
+public class ClaimAchievementRewardCommand extends Command {
+
+    private final Achievement achievement;
+
+    public ClaimAchievementRewardCommand(GameData gameData, Achievement achievement) {
+        super(gameData);
+        this.achievement = achievement;
+    }
+
+    @Override
+    public CommandResult execute() {
+        int reward = achievement.getReward();
+        getPlayer().setCurrentBalance(getPlayer().getCurrentBalance() + reward);
+        getDayManagement().getCurrentDay().incrementDayIncome(reward);
+        getAchievementManagement().updateAchievement(AchievementTypes.MONEY, reward);
+        return new CommandResult("You got " + reward + " FR as reward from \"" + achievement.getName() +"\"", CommandState.DONE);
+    }
 }
