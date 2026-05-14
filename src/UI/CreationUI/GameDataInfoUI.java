3d5fa42761dba9678d98db52b4fffdd4f1b3b944
Maktis <pospisilmatysek@gmail.com>
2026-05-14 17:46:16 +0200

    make EndPanelUI inherit from GameDataInfoUI

--- src/UI/CreationUI/GameDataInfoUI.java
+++ src/UI/CreationUI/GameDataInfoUI.java
@@ -1,4 +1,91 @@
 package UI.CreationUI;
 
-public abstract class GameDataInfoUI {
+import Game.GameData;
+import UI.Exceptions.InvalidUILoadException;
+import Utilities.Important;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static Utilities.Important.initializeScrollPane;
+
+public abstract class GameDataInfoUI extends BackgroundPanel {
+
+    private JPanel mainPanel;
+    private JPanel sidePanel;
+    private final GameData gameData;
+
+    public GameDataInfoUI(GameData gameData) throws InvalidUILoadException {
+        super("/MainUI/ShopUI/STOCK_UI.png");
+        this.gameData = gameData;
+        initialization();
+    }
+
+    protected void initialization() throws InvalidUILoadException {
+        setLayout(new BorderLayout());
+        initializeMainPanel();
+        initializeSidePanel();
+        initializeLayeredPane();
+    }
+
+    private void initializeMainPanel() throws InvalidUILoadException {
+        this.mainPanel = new JPanel();
+        this.mainPanel.setLayout(new BorderLayout());
+        this.mainPanel.setOpaque(false);
+
+        initializeStatistic();
+    }
+
+    private void initializeStatistic() throws InvalidUILoadException {
+        JPanel wrapper = new JPanel();
+        wrapper.setLayout(new BoxLayout(wrapper, BoxLayout.Y_AXIS));
+        wrapper.setOpaque(false);
+        wrapper.setBorder(BorderFactory.createEmptyBorder(0, Important.calculateDimension(300), 0, Important.calculateDimension(300)));
+
+        fillWithStats(wrapper);
+        initializeButton(wrapper);
+
+        JScrollPane scrollPane = initializeScrollPane(wrapper, 16);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder(Important.calculateDimension(135), 0, 0, 0));
+        mainPanel.add(scrollPane, BorderLayout.CENTER);
+    }
+
+    private void fillWithStats(JPanel wrapper){
+        String[] stats = Important.decodeString(this.gameData.toString());
+        for (String s : stats){
+            wrapper.add(Box.createVerticalStrut(Important.calculateDimension(20)));
+            StrokeLabel stat = new StrokeLabel(Important.insertDots(s, 50), 28);
+            stat.setAlignmentX(Component.CENTER_ALIGNMENT);
+            wrapper.add(stat);
+        }
+    }
+
+    public abstract void initializeButton(JPanel wrapper) throws InvalidUILoadException;
+
+    private void initializeSidePanel() throws InvalidUILoadException {
+        this.sidePanel = new JPanel();
+        this.sidePanel.setLayout(new BorderLayout());
+        this.sidePanel.setOpaque(false);
+
+//        BackgroundPanel bar = new BackgroundPanel("/MainUI/ShopUI/END_BAR_UI.png");
+//
+//        Dimension dimension = new Dimension(Important.calculateDimension(Important.getWidth()), Important.calculateDimension(135));
+//        bar.setPreferredSize(dimension);
+//        bar.setMaximumSize(dimension);
+//        bar.setMinimumSize(dimension);
+
+        this.sidePanel.add(initializeBar(), BorderLayout.NORTH);
+    }
+
+    public abstract BackgroundPanel initializeBar() throws InvalidUILoadException;
+
+    private void initializeLayeredPane(){
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setLayout(new OverlayLayout(layeredPane));
+
+        layeredPane.add(mainPanel, JLayeredPane.DEFAULT_LAYER);
+        layeredPane.add(sidePanel, JLayeredPane.PALETTE_LAYER);
+
+        this.add(layeredPane, BorderLayout.CENTER);
+    }
 }
--- src/UI/MainUI/EndGameUI/EndPanelUI.java
+++ src/UI/MainUI/EndGameUI/EndPanelUI.java
@@ -3,9 +3,8 @@ package UI.MainUI.EndGameUI;
 import Game.GameData;
 import UI.CreationUI.BackgroundPanel;
 import UI.CreationUI.CustomButton;
-import UI.CreationUI.StrokeLabel;
+import UI.CreationUI.GameDataInfoUI;
 import UI.Exceptions.InvalidUILoadException;
-import UI.InitialUI.MyFrame;
 import UI.MainUI.MainUI;
 import UI.TitleUI.TitleScreenUI;
 import Utilities.Important;
@@ -13,60 +12,16 @@ import Utilities.Important;
 import javax.swing.*;
 import java.awt.*;
 
-import static Utilities.Important.initializeScrollPane;
+public class EndPanelUI extends GameDataInfoUI {
 
-public class EndPanelUI extends BackgroundPanel {
-
-    private JPanel mainPanel;
-    private JPanel sidePanel;
-    private final GameData gameData;
 
     public EndPanelUI(GameData gameData) throws InvalidUILoadException {
-        super("/MainUI/ShopUI/STOCK_UI.png");
-        this.gameData = gameData;
+        super(gameData);
         initialization();
     }
 
-    private void initialization() throws InvalidUILoadException {
-        setLayout(new BorderLayout());
-        initializeMainPanel();
-        initializeSidePanel();
-        initializeLayeredPane();
-    }
-
-    private void initializeMainPanel() throws InvalidUILoadException {
-        this.mainPanel = new JPanel();
-        this.mainPanel.setLayout(new BorderLayout());
-        this.mainPanel.setOpaque(false);
-
-        initializeStatistic();
-    }
-
-    private void initializeStatistic() throws InvalidUILoadException {
-        JPanel wrapper = new JPanel();
-        wrapper.setLayout(new BoxLayout(wrapper, BoxLayout.Y_AXIS));
-        wrapper.setOpaque(false);
-        wrapper.setBorder(BorderFactory.createEmptyBorder(0, Important.calculateDimension(300), 0, Important.calculateDimension(300)));
-
-        fillWithStats(wrapper);
-        initializeCloseGameButton(wrapper);
-
-        JScrollPane scrollPane = initializeScrollPane(wrapper, 16);
-        scrollPane.setBorder(BorderFactory.createEmptyBorder(Important.calculateDimension(135), 0, 0, 0));
-        mainPanel.add(scrollPane, BorderLayout.CENTER);
-    }
-
-    private void fillWithStats(JPanel wrapper){
-        String[] stats = Important.decodeString(this.gameData.toString());
-        for (String s : stats){
-            wrapper.add(Box.createVerticalStrut(Important.calculateDimension(20)));
-            StrokeLabel stat = new StrokeLabel(Important.insertDots(s, 50), 28);
-            stat.setAlignmentX(Component.CENTER_ALIGNMENT);
-            wrapper.add(stat);
-        }
-    }
-
-    private void initializeCloseGameButton(JPanel wrapper) throws InvalidUILoadException {
+    @Override
+    public void initializeButton(JPanel wrapper) throws InvalidUILoadException {
         CustomButton close = new CustomButton("/MainUI/ShopUI/OK_BUTTON.png", 130, 75);
         close.addActionListener(e ->{
             MainUI mainUI = (MainUI) SwingUtilities.getAncestorOfClass(MainUI.class, this);
@@ -86,11 +41,8 @@ public class EndPanelUI extends BackgroundPanel {
         wrapper.add(close);
     }
 
-    private void initializeSidePanel() throws InvalidUILoadException {
-        this.sidePanel = new JPanel();
-        this.sidePanel.setLayout(new BorderLayout());
-        this.sidePanel.setOpaque(false);
-
+    @Override
+    public BackgroundPanel initializeBar() throws InvalidUILoadException {
         BackgroundPanel bar = new BackgroundPanel("/MainUI/ShopUI/END_BAR_UI.png");
 
         Dimension dimension = new Dimension(Important.calculateDimension(Important.getWidth()), Important.calculateDimension(135));
@@ -98,16 +50,7 @@ public class EndPanelUI extends BackgroundPanel {
         bar.setMaximumSize(dimension);
         bar.setMinimumSize(dimension);
 
-        this.sidePanel.add(bar, BorderLayout.NORTH);
+        return bar;
     }
 
-    private void initializeLayeredPane(){
-        JLayeredPane layeredPane = new JLayeredPane();
-        layeredPane.setLayout(new OverlayLayout(layeredPane));
-
-        layeredPane.add(mainPanel, JLayeredPane.DEFAULT_LAYER);
-        layeredPane.add(sidePanel, JLayeredPane.PALETTE_LAYER);
-
-        this.add(layeredPane, BorderLayout.CENTER);
-    }
 }
