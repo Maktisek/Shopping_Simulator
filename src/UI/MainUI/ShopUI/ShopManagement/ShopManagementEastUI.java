396c03ca554d8f76488d245c426bd398dc3fe5a1
Maktis <pospisilmatysek@gmail.com>
2026-05-18 12:16:17 +0200

    fill ShopManagementEastUI with all needed components

--- src/UI/MainUI/ShopUI/ShopManagement/ShopManagementEastUI.java
+++ src/UI/MainUI/ShopUI/ShopManagement/ShopManagementEastUI.java
@@ -1,6 +1,83 @@
 package UI.MainUI.ShopUI.ShopManagement;
 
-import javax.swing.*;
+import Game.GameData;
+import UI.CreationUI.CustomButton;
+import UI.CreationUI.UpdateAble;
+import UI.DialogUI.NewDayDialogUI;
+import UI.Exceptions.InvalidUILoadException;
+import UI.MainUI.MainUI;
+import UI.MainUI.ShopUI.Days.DayUI;
+import UI.MainUI.ShopUI.Upgrades.UpgradeUI;
+import Upgrade.Utilities.UpgradeNames;
+import Utilities.Important;
 
-public class ShopManagementEastUI extends JPanel {
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class ShopManagementEastUI extends JPanel implements UpdateAble {
+
+    private final GameData gameData;
+    private DayUI dayUI;
+    private final ArrayList<UpgradeUI> upgrades;
+
+    public ShopManagementEastUI(GameData gameData) throws InvalidUILoadException {
+        this.gameData = gameData;
+        this.upgrades = new ArrayList<>();
+
+        initialization();
+    }
+
+    private void initialization() throws InvalidUILoadException {
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.setOpaque(false);
+        this.setBorder(BorderFactory.createEmptyBorder(Important.calculateDimension(18), 0, 0, Important.calculateDimension(28)));
+
+        initializeDay();
+        initializeNewDayButton();
+        initializeUpgrades();
+    }
+
+    private void initializeDay() throws InvalidUILoadException {
+        this.dayUI = new DayUI("/MainUI/ShopUI/DAY_FRAME.png", gameData);
+        dayUI.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        this.add(dayUI);
+    }
+
+    private void initializeNewDayButton() throws InvalidUILoadException {
+        CustomButton nextDay = new CustomButton("/MainUI/ShopUI/NEXT_DAY_BUTTON.png", 200, 85);
+        nextDay.setAlignmentX(Component.CENTER_ALIGNMENT);
+        this.add(Box.createVerticalStrut(Important.calculateDimension(7)));
+        this.add(nextDay);
+
+        nextDay.addActionListener(e -> {
+            MainUI parent = (MainUI) SwingUtilities.getAncestorOfClass(MainUI.class, this);
+            try {
+                parent.showDialog(new NewDayDialogUI("/MainUI/ShopUI/ISSUE_PANE.png", "The tax is  " + Important.parseMoney(gameData.getTax().getCurrent())  + "FR, do you want to proceed into another day?", gameData));
+            } catch (InvalidUILoadException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+    }
+
+    private void initializeUpgrades() throws InvalidUILoadException {
+        this.add(Box.createVerticalStrut(Important.calculateDimension(15)));
+        for (UpgradeNames upgrade : UpgradeNames.values()) {
+            UpgradeUI upgradeUI = new UpgradeUI("/MainUI/ShopUI/ITEM_FRAME.png", gameData.getUpgradeManagement().getUpgrades().get(upgrade), gameData);
+            upgradeUI.setOpaque(false);
+            upgradeUI.setAlignmentX(Component.CENTER_ALIGNMENT);
+            this.add(upgradeUI);
+            this.add(Box.createVerticalStrut(Important.calculateDimension(20)));
+            upgrades.add(upgradeUI);
+        }
+    }
+
+    @Override
+    public void update() throws InvalidUILoadException {
+        this.dayUI.update();
+        for (UpgradeUI upgradeUI : upgrades) {
+            upgradeUI.update();
+        }
+    }
 }
--- src/UI/MainUI/ShopUI/ShopManagement/ShopManagementUI.java
+++ src/UI/MainUI/ShopUI/ShopManagement/ShopManagementUI.java
@@ -3,19 +3,10 @@ package UI.MainUI.ShopUI.ShopManagement;
 import Game.GameData;
 import Shops.Shop;
 import UI.CreationUI.BackgroundPanel;
-import UI.CreationUI.CustomButton;
 import UI.CreationUI.UpdateAble;
 import UI.DialogUI.*;
 import UI.Exceptions.InvalidUILoadException;
-import UI.MainUI.MainUI;
-import UI.MainUI.ShopUI.Bounds.BoundPanelUI;
-import UI.MainUI.ShopUI.Bounds.BoundTypes;
-import UI.MainUI.ShopUI.Days.DayUI;
 import UI.MainUI.ShopUI.ShopUI;
-import UI.MainUI.ShopUI.Upgrades.UpgradeUI;
-import Upgrade.Utilities.UpgradeNames;
-import Utilities.Important;
-
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
@@ -27,18 +18,14 @@ public class ShopManagementUI extends BackgroundPanel implements UpdateAble {
     private final GameData gameData;
     private CardLayout cardLayout;
     private final ArrayList<ShopUI> shopPanels;
-    private BoundPanelUI buyBounds;
-    private BoundPanelUI sellBounds;
-    private DayUI dayUI;
-    private final ArrayList<UpgradeUI> upgrades;
     private ShopManagementNorthUI shopManagementNorthUI;
     private ShopManagementSouthUI shopManagementSouthUI;
+    private ShopManagementEastUI shopManagementEastUI;
 
     public ShopManagementUI(GameData gameData) throws InvalidUILoadException {
         super();
         this.gameData = gameData;
         this.shopPanels = new ArrayList<>();
-        this.upgrades = new ArrayList<>();
         this.cardPanel = new JPanel();
         this.mainPanel = new JPanel(new BorderLayout());
 
@@ -48,6 +35,7 @@ public class ShopManagementUI extends BackgroundPanel implements UpdateAble {
     private void initialize() throws InvalidUILoadException {
         JLayeredPane layeredPane = new JLayeredPane();
         layeredPane.setLayout(new OverlayLayout(layeredPane));
+
         this.cardLayout = new CardLayout();
         cardPanel.setLayout(cardLayout);
         setLayout(new BorderLayout());
@@ -91,57 +79,12 @@ public class ShopManagementUI extends BackgroundPanel implements UpdateAble {
 
     private void initializeSouthPanel(JPanel panel) throws InvalidUILoadException {
         this.shopManagementSouthUI = new ShopManagementSouthUI(gameData);
-
         panel.add(shopManagementSouthUI, BorderLayout.SOUTH);
     }
 
-
     private void initializeEast(JPanel panel) throws InvalidUILoadException {
-        JPanel eastPanel = new JPanel();
-        eastPanel.setLayout(new BoxLayout(eastPanel, BoxLayout.Y_AXIS));
-        eastPanel.setOpaque(false);
-        eastPanel.setBorder(BorderFactory.createEmptyBorder(Important.calculateDimension(18), 0, 0, Important.calculateDimension(28)));
-
-        initializeDay(eastPanel);
-        initializeNewDayButton(eastPanel);
-        initializeUpgrades(eastPanel);
-
-        panel.add(eastPanel, BorderLayout.EAST);
-    }
-
-    private void initializeDay(JPanel eastPanel) throws InvalidUILoadException {
-        this.dayUI = new DayUI("/MainUI/ShopUI/DAY_FRAME.png", gameData);
-        dayUI.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-        eastPanel.add(dayUI);
-    }
-
-    private void initializeNewDayButton(JPanel panel) throws InvalidUILoadException {
-        CustomButton nextDay = new CustomButton("/MainUI/ShopUI/NEXT_DAY_BUTTON.png", 200, 85);
-        nextDay.setAlignmentX(Component.CENTER_ALIGNMENT);
-        panel.add(Box.createVerticalStrut(Important.calculateDimension(7)));
-        panel.add(nextDay);
-
-        nextDay.addActionListener(e -> {
-            MainUI parent = (MainUI) SwingUtilities.getAncestorOfClass(MainUI.class, this);
-            try {
-                parent.showDialog(new NewDayDialogUI("/MainUI/ShopUI/ISSUE_PANE.png", "The tax is  " + Important.parseMoney(gameData.getTax().getCurrent())  + "FR, do you want to proceed into another day?", gameData));
-            } catch (InvalidUILoadException ex) {
-                throw new RuntimeException(ex);
-            }
-        });
-    }
-
-    private void initializeUpgrades(JPanel eastPanel) throws InvalidUILoadException {
-        eastPanel.add(Box.createVerticalStrut(Important.calculateDimension(15)));
-        for (UpgradeNames upgrade : UpgradeNames.values()) {
-            UpgradeUI upgradeUI = new UpgradeUI("/MainUI/ShopUI/ITEM_FRAME.png", gameData.getUpgradeManagement().getUpgrades().get(upgrade), gameData);
-            upgradeUI.setOpaque(false);
-            upgradeUI.setAlignmentX(Component.CENTER_ALIGNMENT);
-            eastPanel.add(upgradeUI);
-            eastPanel.add(Box.createVerticalStrut(Important.calculateDimension(20)));
-            upgrades.add(upgradeUI);
-        }
+        this.shopManagementEastUI = new ShopManagementEastUI(gameData);
+        panel.add(this.shopManagementEastUI, BorderLayout.EAST);
     }
 
     private void initializeNorth(JPanel wrapper) throws InvalidUILoadException {
@@ -158,13 +101,8 @@ public class ShopManagementUI extends BackgroundPanel implements UpdateAble {
         for (ShopUI shopPanel : shopPanels) {
             shopPanel.update();
         }
-        this.dayUI.update();
-        for (UpgradeUI upgradeUI : upgrades) {
-            upgradeUI.update();
-        }
         this.shopManagementNorthUI.update();
         this.shopManagementSouthUI.update();
+        this.shopManagementEastUI.update();
     }
-
-
 }
