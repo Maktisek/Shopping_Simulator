c3447d1c05f7017fd8f0610e56d73122abd6c58b
Maktis <pospisilmatysek@gmail.com>
2026-05-25 12:22:08 +0200

    add documentation for - ShopDirection.java

--- src/Commands/ShopCommands/ShopDirection.java
+++ src/Commands/ShopCommands/ShopDirection.java
@@ -1,7 +1,8 @@
 package Commands.ShopCommands;
 
 /**
- * This enum works as a "direction" identifier for {@link BuyShopCommand}
+ * This enum works as a "direction" identifier for both {@link BuyShopCommand} and {@link ChangeShopCommand}
+ * @author Matěj Pospíšil
  */
 public enum ShopDirection {
 
