116bfe84b606caa1b7e073692b9711ce234babaa
Maktis <pospisilmatysek@gmail.com>
2026-05-25 12:46:12 +0200

    add documentation for - CommandState.java

--- src/Commands/CommandState.java
+++ src/Commands/CommandState.java
@@ -1,5 +1,12 @@
 package Commands;
 
+/**
+ * This enum represents different command states.
+ * <p>
+ *     {@link CommandResult} has its own state represented by this enum.
+ * </p>
+ * @author Matěj Pospíšil
+ */
 public enum CommandState {
 
     DONE, FAILED_ISSUE, FAILED_BUY, FAILED_END
