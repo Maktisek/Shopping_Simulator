3bb530ad006d8c9e172a858e95ecd26b054cae7a
Maktis <pospisilmatysek@gmail.com>
2026-05-25 09:10:45 +0200

    add documentation for - AudioType.java

--- src/Achievements/AchievementType.java
+++ src/Achievements/AchievementType.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 /**
  * Stands for various types of achievements.
  * Achievements are stored based on those types.
+ * @author Matěj Pospíšil
  */
 public enum AchievementType implements Serializable {
 
--- src/AudioSystem/AudioType.java
+++ src/AudioSystem/AudioType.java
@@ -1,5 +1,12 @@
 package AudioSystem;
 
+/**
+ * Represents types of audios.
+ * <p>
+ *     Used in order to store music and sounds separately.
+ * </p>
+ * @author Matěj Pospíšil
+ */
 public enum AudioType {
 
     MUSIC, SOUNDS
