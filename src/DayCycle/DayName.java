efd30f11f8129e68acc475745bbee247091c1dc2
Maktis <pospisilmatysek@gmail.com>
2026-05-25 13:07:37 +0200

    add documentation for - DayName.java

--- src/DayCycle/DayName.java
+++ src/DayCycle/DayName.java
@@ -2,6 +2,11 @@ package DayCycle;
 
 import java.io.Serializable;
 
+/**
+ * This enum represents day names.
+ * That is all:)
+ * @author Matěj Pospíšil
+ */
 public enum DayName implements Serializable {
     Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
 }
