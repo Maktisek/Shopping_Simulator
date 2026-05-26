d63de1243f16b50fdea363e32fa22258aa2ed1af
Maktis <pospisilmatysek@gmail.com>
2026-05-26 10:56:37 +0200

    add documentation for - ItemNPC.java

--- src/Items/ItemNPC.java
+++ src/Items/ItemNPC.java
@@ -6,6 +6,18 @@ import Utilities.Important;
 
 import java.io.Serializable;
 
+/**
+ * This class stands for an item stored in an instance of {@link NPCs.NPC}.
+ * <p>
+ *     It is a simple class featuring:
+ *     <ul>
+ *         <li>{@link #item} as the main product</li>
+ *         <li>{@link #amountManager} as the amount counter</li>
+ *     </ul>
+ * </p>
+ * Here it is clearly visible, why the system with {@link ItemBase} works so well
+ * @author Matěj Pospíšil
+ */
 public class ItemNPC implements Item, Serializable {
 
     private ItemBase item;
