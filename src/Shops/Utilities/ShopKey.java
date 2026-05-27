c313812f2f72fa2b8432c58c05cce5c61ef60885
Maktis <pospisilmatysek@gmail.com>
2026-05-27 07:33:16 +0200

    add documentation for - ShopKey.java

--- src/Shops/Utilities/ShopKey.java
+++ src/Shops/Utilities/ShopKey.java
@@ -2,6 +2,21 @@ package Shops.Utilities;
 
 import java.io.Serializable;
 
+/**
+ * This class represents a system for shop unlocking.
+ * <p>
+ *     It manages whether the player can reach that shop or not.
+ * </p>
+ * <p>
+ *     <ul>
+ *         <li>{@link #price} stands for how much does the shop costs</li>
+ *         <li>{@link #unlocked} represents a boolean value, which determines if the shop has been already unlocked</li>
+ *         <li>{@link #rebirthLevel} tells how big level player has to be in order to unlock or reach this shop. This value has the biggest
+ *         preference when it comes to checking if the player can change the shop</li>
+ *     </ul>
+ * </p>
+ * @author Matěj Pospíšil
+ */
 public class ShopKey implements Serializable {
 
     private int price;
