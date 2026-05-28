d1ff311de02511d35e18ca87964790bc225ec0a6
Maktis <pospisilmatysek@gmail.com>
2026-05-28 14:46:11 +0200

    add documentation for - MyFrame.java

--- src/UI/InitialUI/MyFrame.java
+++ src/UI/InitialUI/MyFrame.java
@@ -12,6 +12,14 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowStateListener;
 
+/**
+ * This class represents the main frame where the main game is being displayed.
+ * <p>
+ *     It extends {@link FrameBaseUI} so it behaves like a {@link JFrame}.
+ * </p>
+ * For initializing {@link #mainUI} a proper instance of {@link GameData} has to be inserted.
+ * If the given instance of {@link GameData} is not filled by data, the window will not open.
+ */
 public class MyFrame extends FrameBaseUI {
 
     private final GameData gameData;
@@ -49,6 +57,14 @@ public class MyFrame extends FrameBaseUI {
         getContentPane().add(this.mainUI, BorderLayout.CENTER);
     }
 
+    /**
+     * This method initializes a window state listener.
+     * <p>
+     *     This window state listener listens to any behaviour of this frame.
+     *     If there is any action with the frame and the action is not minimizing the window,
+     *     method {@link #refreshUI()} is executed.
+     * </p>
+     */
     private void initializeWindowOperations() {
         this.addWindowStateListener(new WindowAdapter() {
             @Override
@@ -60,6 +76,15 @@ public class MyFrame extends FrameBaseUI {
         });
     }
 
+    /**
+     * This method refreshes the window into "fullscreen", even tough it is not actual fullscreen.
+     * <p>
+     *     Well, I was trying to find a solution, how to simulate fullscreen without it actually being full screen, because when
+     *     actual full screen was on, when changing resolution it was making a mess.
+     * </p>
+     * This solution works pretty well. When the window is minimized and then again opened, the window resets its position
+     * and spreads across the whole screen. Otherwise, the window would be unopenable after minimizing it.
+     */
     private void refreshFullscreen() {
         setLocationRelativeTo(null);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
