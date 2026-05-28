a0513892f8013ba73e1bbe804c323a38a8729ccb
Maktis <pospisilmatysek@gmail.com>
2026-05-28 07:24:55 +0200

    add documentation for - MaxSoldUpgrade.java

--- src/Upgrade/Upgrades/MaxSoldUpgrade.java
+++ src/Upgrade/Upgrades/MaxSoldUpgrade.java
@@ -2,7 +2,14 @@ package Upgrade.Upgrades;
 
 import Upgrade.UpgradeBasicType;
 import Upgrade.Utilities.UpgradeNames;
-
+/**
+ * This class represents an upgrade extending {@link UpgradeBasicType}.
+ * <p>
+ *     Upgrade type is {@link UpgradeNames#SELL}
+ * </p>
+ * @author Matěj Pospíšil
+ * @since   1.0 - (pre-release version)
+ */
 public class MaxSoldUpgrade extends UpgradeBasicType {
 
     public MaxSoldUpgrade() {
