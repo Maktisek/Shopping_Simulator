5c4706a886c4b2a220d6fc54068112c83a13a893
Maktis <pospisilmatysek@gmail.com>
2026-05-28 18:35:09 +0200

    add documentation for - AchievementUITypes.java

--- src/UI/MainUI/AchievementUI/AchievementUITypes.java
+++ src/UI/MainUI/AchievementUI/AchievementUITypes.java
@@ -1,5 +1,10 @@
 package UI.MainUI.AchievementUI;
 
+/**
+ * This enum represents two different states of {@link AchievementBoxUI}.
+ * @author Matěj Pospíšil
+ * @since   1.0 - (pre-release version)
+ */
 public enum AchievementUITypes {
 
     POSSIBLE, DONE
