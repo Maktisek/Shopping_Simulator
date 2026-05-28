f82d2a209ec178bc5805679cfa2be288fe02e7d8
Maktis <pospisilmatysek@gmail.com>
2026-05-28 07:19:27 +0200

    add documentation for - Upgrade.java

--- src/Upgrade/Upgrade.java
+++ src/Upgrade/Upgrade.java
@@ -2,12 +2,51 @@ package Upgrade;
 
 import Upgrade.Utilities.UpgradeNames;
 
+/**
+ * This interface represents an upgrade.
+ * <p>
+ *     Every upgrade in the game has to implement this interface in order to be stored and manipulated correctly.
+ * </p>
+ * <p>
+ *     Although, it is recommended creating an abstract class and implementing this interface there.
+ *     This is completely optional.
+ * </p>
+ * @author Matěj Pospíšil
+ * @since   1.0 - (pre-release version)
+ */
 public interface Upgrade {
 
+    /**
+     * This method levels up the upgrade.
+     */
     void levelUp();
+
+    /**
+     * This method changes price of the upgrade.
+     */
     void changePrice();
+
+    /**
+     * Getter for inner data.
+     * @return the data
+     */
     int dataInfo();
+
+    /**
+     * Getter for price.
+     * @return the price
+     */
     int priceInfo();
+
+    /**
+     * Getter for the level
+     * @return the level
+     */
     int levelInfo();
+
+    /**
+     * Getter for the upgrade type
+     * @return the upgrade type
+     */
     UpgradeNames nameInfo();
 }
