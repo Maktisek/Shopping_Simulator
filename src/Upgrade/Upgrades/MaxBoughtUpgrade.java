e285d1469ac78e1a09f61378c48ee261b764df4b
Maktis <pospisilmatysek@gmail.com>
2026-05-28 07:24:05 +0200

    add documentation for - MaxBoughtUpgrade.java

--- src/Upgrade/Upgrades/MaxBoughtUpgrade.java
+++ src/Upgrade/Upgrades/MaxBoughtUpgrade.java
@@ -3,6 +3,14 @@ package Upgrade.Upgrades;
 import Upgrade.UpgradeBasicType;
 import Upgrade.Utilities.UpgradeNames;
 
+/**
+ * This class represents an upgrade extending {@link UpgradeBasicType}.
+ * <p>
+ *     Upgrade type is {@link UpgradeNames#BUY}
+ * </p>
+ * @author Matěj Pospíšil
+ * @since   1.0 - (pre-release version)
+ */
 public class MaxBoughtUpgrade extends UpgradeBasicType {
 
     public MaxBoughtUpgrade() {
