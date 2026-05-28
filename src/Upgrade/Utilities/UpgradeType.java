c1e3de5470b5786317491bf402539a2e6a3b813a
Maktis <pospisilmatysek@gmail.com>
2026-05-28 09:50:42 +0200

    add documentation for - UpgradeType.java

--- src/Upgrade/Utilities/UpgradeType.java
+++ src/Upgrade/Utilities/UpgradeType.java
@@ -1,5 +1,13 @@
 package Upgrade.Utilities;
 
+/**
+ * This enum stands for individual types of upgrades.
+ * <p>
+ *     Every upgrade in the game must have its own type.
+ * </p>
+ * @author Matěj Pospíšil
+ * @since   1.0 - (pre-release version)
+ */
 public enum UpgradeType {
     BUY, SELL, STOCK
 }
--- src/Utilities/Exceptions/WrongIntervalException.java
+++
@@ -1,7 +0,0 @@
-package Utilities.Exceptions;
-
-public class WrongIntervalException extends Exception {
-    public WrongIntervalException(String message) {
-        super(message);
-    }
-}
