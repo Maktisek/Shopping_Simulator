e758420866b5b0b55b115398233988ae8bd3c79d
Maktis <pospisilmatysek@gmail.com>
2026-05-28 19:05:23 +0200

    add documentation for - ItemSpecification.java

--- src/UI/MainUI/ShopUI/Items/ItemSpecification.java
+++ src/UI/MainUI/ShopUI/Items/ItemSpecification.java
@@ -1,5 +1,13 @@
 package UI.MainUI.ShopUI.Items;
 
+/**
+ * This enum represents an origin of an individual instance of {@link Items.Item}.
+ * <p>
+ *     It is used in {@link ItemUI} for proper and clean implementation of initialization.
+ * </p>
+ * @author Matěj Pospíšil
+ * @since   1.0 - (pre-release version)
+ */
 public enum ItemSpecification {
     SHOP, NPC
 }
